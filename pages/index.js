7d6379c428ce7873c5a35fa13ded702ec7146c99
paperboatclub <148582079+paperboatclub@users.noreply.github.com>
2024-08-06 12:11:19 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Marmar's Kitchen - Freshly fucked up food straight to your plate</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Alreet" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
